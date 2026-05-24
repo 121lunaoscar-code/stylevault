e6bdb93c362f939f41440a1042d40b9122745cee
121lunaoscar-code <121lunaoscar@gmail.com>
2026-05-24 18:09:39 -0500

    Delete src/App.tsx

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-export default function App() {
-  return (
-    <div style={{background:"#060606",minHeight:"100vh",display:"flex",alignItems:"center",justifyContent:"center"}}>
-      <h1 style={{color:"#b8965a",fontFamily:"serif",fontSize:"40px",letterSpacing:"8px"}}>STYLEVAULT</h1>
-    </div>
-  );
-}
